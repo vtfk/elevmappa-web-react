604bace19f984e691a7649740cd615d5706a65f2
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2022-08-03 13:11:00 +0200

    Add VTFK base style

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BaseStyle } from '@vtfk/components'
 import './index.css'
 import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <BaseStyle>
+      <App />
+    </BaseStyle>
   </React.StrictMode>
 )
