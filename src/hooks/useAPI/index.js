b15ded21cdc8fe90e86f9b1a01535da1e03f6dd1
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2022-08-09 14:15:44 +0200

    Added `getFile` to retrieve a specific file

--- src/hooks/useAPI/index.js
+++ src/hooks/useAPI/index.js
@@ -1,11 +1,12 @@
 import { useEffect, useMemo, useState } from 'react'
 import { useSession } from '@vtfk/react-msal'
 import { orderBy } from 'lodash'
+import { hasStudent } from '../../lib/has-data'
 
 import { API } from '../../config'
 
-export function useAPI (endpoint, itemProps = []) {
-  const { apiGet } = useSession()
+export function useAPI (endpoint, defaultOrderBy, defaultOrder, itemProps = []) {
+  const { apiGet, apiPost } = useSession()
   const [_items, setItems] = useState([])
   const [itemsOptions, setItemsOptions] = useState({})
   const [loading, setLoading] = useState(false)
@@ -17,7 +18,7 @@ export function useAPI (endpoint, itemProps = []) {
         const data = await apiGet(`${API.URL}/${endpoint}`)
         setItems(data)
       } catch (error) {
-        console.log('Failed to get items from', endpoint)
+        console.error('Failed to get items from', endpoint)
         // TODO: use Sentry here
         setItems([])
       }
@@ -27,23 +28,51 @@ export function useAPI (endpoint, itemProps = []) {
     getItems()
   }, [endpoint]) // eslint-disable-line
 
+  const getFile = async (source, documentId, recno, id) => {
+    try {
+      const data = await apiPost(`${API.URL}/file`, { source, documentId, recno, id }, true)
+      console.log('I got:', data)
+      return data.data
+    } catch (error) {
+      console.error('Failed to fetch file', documentId, 'with recno', recno, `from '${source}'`)
+      // TODO: use Sentry here
+      return null
+    }
+  }
+
   const options = useMemo(() => {
     return {
       filter: '',
-      orderBy: ['fullName'],
-      order: 'asc',
+      orderBy: [defaultOrderBy],
+      order: defaultOrder,
       ...itemsOptions
     }
-  }, [itemsOptions])
+  }, [itemsOptions, defaultOrderBy, defaultOrder])
 
   const items = useMemo(() => {
-    if (options.filter === '') return orderBy(_items, options.orderBy, options.order)
+    const orderItems = items => orderBy(items, options.orderBy, options.order)
+
+    if (hasStudent(_items)) return _items
+    if (options.filter === '') return orderItems(_items)
 
     const filtered = _items.filter(item => itemProps.map(prop => item[prop].toLowerCase().includes(options.filter.toLowerCase())).includes(true))
-    return orderBy(filtered, options.orderBy, options.order)
+    return orderItems(filtered)
+  }, [_items, itemProps, options])
+
+  const documents = useMemo(() => {
+    const orderDocuments = documents => orderBy(documents, [resultItem => new Date(resultItem.date)], options.order)
+
+    if (!hasStudent(_items)) return []
+
+    if (options.filter === '') return orderDocuments(_items.documents)
+
+    const filtered = _items.documents.filter(document => itemProps.map(prop => document[prop].toLowerCase().includes(options.filter.toLowerCase())).includes(true))
+    return orderDocuments(filtered)
   }, [_items, itemProps, options])
 
   return {
+    documents,
+    getFile,
     items,
     itemsOptions,
     loading,
