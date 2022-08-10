0e9503e246de49e8dec479c85427f562f4429f49
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2022-08-10 09:58:14 +0200

    Make sure `items.documents` is an array

--- src/lib/has-data.js
+++ src/lib/has-data.js
@@ -1,4 +1,4 @@
 export const hasStudent = items => !Array.isArray(items) && Object.getOwnPropertyNames(items).length > 0
-export const hasDocuments = items => hasStudent(items) && items.documents.length > 0
+export const hasDocuments = items => hasStudent(items) && Array.isArray(items.documents) && items.documents.length > 0
 
 export const hasStudents = items => Array.isArray(items) && items.length > 0
